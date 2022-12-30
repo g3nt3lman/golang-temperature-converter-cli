3960d1ab1f0034e3b29c0cb9c1d60636f87a2d82
marcin.graczyk <marcin.graczyk@shopee.com>
2022-12-30 19:23:29 +0100

    comm2

--- main.go
+++ main.go
@@ -22,9 +22,17 @@ func main() {
 		printError(errInvalidArguments)
 	}
 	originUnit = strings.ToUpper(os.Args[1])
+	if originUnit == "C" {
+		convertToFahrenheit(originValue)
+	} else {
+		convertToCelsius(originValue)
+	}
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-
+		_, err := fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
 		if shouldConvertAgain != "Y" {
